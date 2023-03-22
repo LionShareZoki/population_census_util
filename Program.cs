6d40a3bd5203b69b1e0fd7044c0c8408ae98246f
Zoran <pujic.zoran@gmail.com>
2023-03-22 22:13:26 +0100

    add loopTheInput function, finish getInputData function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿while (true)
+﻿string[] genders;
+int[] ages = new int[0];
+double[] incomes = new double[0];
+
+while (true)
 {
     Console.WriteLine("Menu:");
     Console.WriteLine("1. InputData");
@@ -6,17 +10,59 @@
     Console.WriteLine("3. Exit");
 
     Console.Write("Enter your choice: ");
-    string input = Console.ReadLine();
+    string input = Console.ReadLine()!;
     bool isNumber = int.TryParse(input, out int number);
 
     if (isNumber)
     {
         if (number == (int)MenuOption.InputData)
         {
-            Console.WriteLine("yo");
+            Console.WriteLine("How many people will be interviewed?");
+            int numberOfPeople = int.Parse(Console.ReadLine()!);
+            LoopTheInput(numberOfPeople);
+
         }
     }
 }
+
+void LoopTheInput(int numberOfPeople)
+{
+    for (int i = 0; i < numberOfPeople; i++)
+    {
+        GetInputData();
+    }
+    throw new NotImplementedException();
+
+}
+
+static void GetInputData()
+{
+    Console.Clear();
+    Console.WriteLine("Tell me about yourself:");
+
+    Console.Write("Enter gender: ");
+    string gender = Console.ReadLine()!;
+    string[] genders = new string[1];
+    genders[0] = gender;
+   
+
+    Console.Write("Enter age: ");
+    string ageInput = Console.ReadLine()!;
+    int age = int.Parse(ageInput);
+    int[] ages = new int[1];
+    ages[0] = age;
+    
+
+
+    Console.Write("Enter income: ");
+    string incomeInput = Console.ReadLine()!;
+    double income = double.Parse(incomeInput);
+    double[] incomes = new double[1];
+    incomes[0] = income;
+
+}
+
+
 
 public enum MenuOption
 {
