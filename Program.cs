ff14748b51c870a06dd2117dc33dd75aa19fea6f
Zoran <pujic.zoran@gmail.com>
2023-03-22 20:57:38 +0100

    create sub menu enum CensusOption

--- Program.cs
+++ Program.cs
@@ -6,3 +6,22 @@
     Exit = 3
 
 }
+
+    public enum CensusOption
+{
+    GenderRatio = 1,
+    AgePyramid = 2,
+    AverageAge = 3,
+    AverageIncome = 4
+}
+
+while(true)
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1. Census");
+    Console.WriteLine("2. Average Age");
+    Console.WriteLine("3. Exit");
+
+    Console.Write("Enter your choice: ");
+    string input = Console.ReadLine();
+}
