48de45772aeeadcd1cf60ec03ab12ab3ad944b91
Zoran <pujic.zoran@gmail.com>
2023-03-22 21:30:26 +0100

    Create GetInputData function

--- Program.cs
+++ Program.cs
@@ -1,27 +1,34 @@
-﻿
-    public enum MenuOption
+﻿while (true)
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1. InputData");
+    Console.WriteLine("2. Census");
+    Console.WriteLine("3. Exit");
+
+    Console.Write("Enter your choice: ");
+    string input = Console.ReadLine();
+    bool isNumber = int.TryParse(input, out int number);
+
+    if (isNumber)
+    {
+        if (number == (int)MenuOption.InputData)
+        {
+            Console.WriteLine("yo");
+        }
+    }
+}
+
+public enum MenuOption
 {
     InputData = 1,
     Census = 2,
     Exit = 3
-
 }
 
-    public enum CensusOption
+public enum CensusOption
 {
     GenderRatio = 1,
     AgePyramid = 2,
     AverageAge = 3,
     AverageIncome = 4
-}
-
-while(true)
-{
-    Console.WriteLine("Menu:");
-    Console.WriteLine("1. Census");
-    Console.WriteLine("2. Average Age");
-    Console.WriteLine("3. Exit");
-
-    Console.Write("Enter your choice: ");
-    string input = Console.ReadLine();
 }
